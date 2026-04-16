34b56594569116c8c746f00448d63d7272d9c5fb
Umesh <yesireddyumesh@gmail.com>
2026-04-16 18:08:17 +0530

    final server fix

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,19 @@ const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/trains", trainRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on ${import.meta.env.VITE_API_URL}");
+// Server start
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
