4a9df465299e6468096e9aa3895c93e912aefb08
Umesh <yesireddyumesh@gmail.com>
2026-04-16 17:57:46 +0530

    env setup done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "nozomi.proxy.rlwy.net",
-  user: "root",
-  password: "yDDrhWVlZEQCLJFUpqxPAiiaNTpChHVc",
-  database: "railway",
-  port: 33622
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
